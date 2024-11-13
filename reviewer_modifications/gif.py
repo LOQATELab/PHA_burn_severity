0e434ee9f399300f63cea71dce1622087ae0dcfa
Samuel Kalevi Zebarth Taylor <116521490+Sam-Taylor-UIOWA@users.noreply.github.com>
2024-11-13 11:14:42 -0600

    Update gif.py

--- reviewer_modifications/gif.py
+++ reviewer_modifications/gif.py
@@ -7,15 +7,15 @@ import geopandas as gpd
 
 #basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/OneDrive_1_11-7-2024/TF_TP-basins.shp'
 #basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/TF_FP-basins.shp'
-basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/TF_I_NE.shp'
-#basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/TF_TN_Final-basins.shp'
+#basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/TF_I_NE.shp'
+basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/TF_TN_Final-basins.shp'
 #basin_path = r'/Volumes/PhD/imagery/masters/TF_basin-TPN/TF_FN-basins.shp'
 
 basins = gpd.read_file(basin_path)
 basins = basins['BASIN_ID']
 
 path = r'/Volumes/PhD/imagery/masters/output/global/TPN-basins/'
-out = r'/Volumes/PhD/imagery/masters/gif/Init-NE/'
+out = r'/Volumes/PhD/imagery/masters/gif/True-Negative/'
 def make_gif(frame_folder, n):
     frames = [Image.open(image) for image in b]
     frame_one = frames[0]
@@ -23,7 +23,7 @@ def make_gif(frame_folder, n):
                save_all=True, duration=200, loop=0)
 
 for basin in basins:
-    n = 'I_NE'
-    b = glob.glob(f'{path}/*{basin}**I_NE.png')
+    n = 'TN'
+    b = glob.glob(f'{path}/*{basin}**TN.png')
     b.sort()
     make_gif(b, n)
